a761ef78182c2e71bd1ed53669499dae45e70455
MuslimMujahid <muslim.mujahid22@gmail.com>
2025-05-30 01:15:52 +0700

    working with shadcn

--- apps/teacher-client/src/app/page.tsx
+++ apps/teacher-client/src/app/page.tsx
@@ -1,10 +1,11 @@
-import { SharedComponent } from '@one-exam-monorepo/ui';
+import { Input, SharedComponent } from '@one-exam-monorepo/ui';
 
 export default function Index() {
   return (
     <div>
       <SharedComponent />
       <div className='text-green-500'>This text should be green</div>
+      <Input />
     </div>
   );
 }
--- shared/ui/src/index.ts
+++ shared/ui/src/index.ts
@@ -1,1 +1,2 @@
 export * from './lib/shared-component';
+export * from './lib/input/input';
